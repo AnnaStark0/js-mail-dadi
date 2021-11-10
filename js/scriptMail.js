c28d36c3df16b88ca6047c04375c187db8190280
Annastark <89winteriscoming@gmail.com>
2021-11-10 15:48:02 +0100

    array + ciclo

--- js/scriptMail.js
+++ js/scriptMail.js
@@ -2,7 +2,22 @@
 
 
 // 1-Chiedi all'utente la sua email
-const mail = prompt ('Inserisci la tua email');
-console.log (mail);
+const tuaMail = prompt('Inserisci la tua email');
+console.log(tuaMail);
 
 // 2-Controlla che sia nella lista di chi può accedere
+const email = ['@gmail.com', '@libero.it', '@hotmail.com'];
+
+let sentinella = false;
+
+for (let i = 0; i < email.length; i++) {
+    if (email[i] == tuaMail) {
+        sentinella = true;
+    }
+}
+
+if (sentinella) {
+    console.log('Accesso');
+    } else {
+        console.log ('accesso negato');
+    }
