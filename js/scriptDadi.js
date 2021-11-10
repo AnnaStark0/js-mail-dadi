b809173f0eb194a0d214b39798dfa70a028005e1
Annastark <89winteriscoming@gmail.com>
2021-11-10 16:53:47 +0100

    Stampa Numero Random

--- js/scriptDadi.js
+++ js/scriptDadi.js
@@ -1,6 +1,15 @@
 //Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 //1-Genera un Numero Random da 1 a 6
-let num = Math.floor(Math.random() * 10);
+let numPc = Math.floor(Math.random() * 6) + 1;
 
-console.log (num);
+//Many hours later........
+
+document.getElementById('resultPc').innerHTML = numPc ;
+console.log (numPc);
+
+
+
+
+// il pc genera un num random,
+// fare funzione, memorizzare numero. dopo di che si apre il prompt x il giocatore
