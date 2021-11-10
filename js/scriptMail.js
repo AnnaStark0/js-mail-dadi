66c0243ea469ac6e7b642c87f65e5ef3e4d0d0cd
Annastark <89winteriscoming@gmail.com>
2021-11-10 15:29:58 +0100

    prompt

--- js/scriptMail.js
+++ js/scriptMail.js
@@ -1,1 +1,8 @@
 // Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+// 1-Chiedi all'utente la sua email
+const mail = prompt ('Inserisci la tua email');
+console.log (mail);
+
+// 2-Controlla che sia nella lista di chi può accedere
