b9574320063ffc0130cf9c9122e78d8d44e6e2c2
Annastark <89winteriscoming@gmail.com>
2021-11-10 16:59:49 +0100

    Stampa Num Computer + Num Giocatore

--- js/scriptDadi.js
+++ js/scriptDadi.js
@@ -1,12 +1,17 @@
 //Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-//1-Genera un Numero Random da 1 a 6
+//1-Genera un Numero Random da 1 a 6 - Computer
 let numPc = Math.floor(Math.random() * 6) + 1;
-
-//Many hours later........
 
 document.getElementById('resultPc').innerHTML = numPc ;
 console.log (numPc);
+//Many hours later........
+
+//1-Genera un Numero Random da 1 a 6 - Giocatore
+let numGiocatore = Math.floor(Math.random() * 6) + 1;
+
+document.getElementById('resultGioc').innerHTML = numGiocatore ;
+console.log (numGiocatore);
 
 
 
