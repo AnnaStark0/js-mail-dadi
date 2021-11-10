04435a5a79558ffa289798745709ad6a843886fa
Annastark <89winteriscoming@gmail.com>
2021-11-10 18:56:20 +0100

    Fine entrambi gli Esercizi.

--- js/scriptDadi.js
+++ js/scriptDadi.js
@@ -1,6 +1,9 @@
 //Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+
 //1-Genera un Numero Random da 1 a 6 - Computer
+
 let numPc = Math.floor(Math.random() * 6) + 1;
 
 document.getElementById('resultPc').innerHTML = numPc ;
@@ -8,19 +11,14 @@ console.log (numPc);
 //Many hours later........
 
 //1-Genera un Numero Random da 1 a 6 - Giocatore
+
 let numGiocatore = Math.floor(Math.random() * 6) + 1;
 
 document.getElementById('resultGioc').innerHTML = numGiocatore ;
 console.log (numGiocatore);
 
-
-
 //2-Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-//let vincitore;
-
-
-//PROVA 2-
 if (numPc > numGiocatore) {
     console.log('Ha vinto il computer!');
     document.getElementById('result').innerHTML = 'Ha vinto il computer!' ;
@@ -30,3 +28,6 @@ if (numPc > numGiocatore) {
         document.getElementById('result').innerHTML = 'Hai Vinto!' ;
     }
 }
+
+
+//Promemoria: Non ho inserito la condizione di uguaglianza fra i due risultati!
